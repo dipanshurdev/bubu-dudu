ce27f33c84e8030c2dd1bf88f7cf307e2cd7d765
Aman <dv451197@gmail.com>
2024-02-03 22:25:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+
+import "./index.css";
+import { DubuCry, Sorries } from "./support";
+
+function App() {
+  const [yes, setYes] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [sorryIdx, setSorryIdx] = useState(0);
+  const [widthPrsnt, setWidthPrsnt] = useState(1);
+
+  const saidNo = () => {
+    if (!yes) {
+      setIndex(index + 1);
+      setSorryIdx(sorryIdx + 1);
+      setWidthPrsnt(widthPrsnt + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (index >= DubuCry.length) {
+      setIndex(0);
+    }
+    if (sorryIdx >= Sorries.length) {
+      setSorryIdx(0);
+    }
+  }, [index]);
+
+  console.log(index);
+
+  return (
+    <>
+      <div className="h-screen  w-full p-4">
+        <div className="flex items-center justify-center w-full flex-col ">
+          <div className="gif-center flex flex-col items-center justify-center mt-10">
+            <img
+              src={yes ? "src/assets/gifs/maangoi-bubu.gif" : DubuCry[index]}
+              alt="dudu-crying"
+              className="h-80 w-80 object-contain rounded-2xl"
+            />
+            {!yes && (
+              <div className="flex flex-col justify-center items-center text-center">
+                <h1 className="mt-2 text-gray-600 font-semibold text-xl">
+                  Please mujhe chhama🙇‍♂️ kardo na 🥺🥺.
+                </h1>
+                <span className=" text-gray-600 font-normal text-base">
+                  Tum hi galat sochti ho😑, aur serious ho jati ho.
+                </span>
+                <span className=" text-gray-600 font-semibold text-lg">
+                  Per Fir bhi🥺
+                </span>
+                <big className="text-gray-600 font-semibold text-lg">Sorry</big>
+              </div>
+            )}
+          </div>
+          {!yes && (
+            <div className="w-full flex items-center justify-center mt-2 flex-col gap-2">
+              <button
+                onClick={() => setYes(true)}
+                className="px-3 py-2 bg-blue-400 rounded-xl text-white w-30"
+                id="yess"
+                style={{ fontSize: `${widthPrsnt}rem` }}
+              >
+                Thik hai ji🥰
+              </button>
+              <button
+                onClick={() => saidNo()}
+                className="px-3 py-2 bg-red-400 rounded-xl text-white w-30"
+              >
+                {Sorries[sorryIdx]}
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/support/index.js
@@ -0,0 +1,19 @@
+export const DubuCry = [
+  "src/assets/gifs/crying-dudu.gif",
+  "src/assets/gifs/dudu-crying2.gif",
+  "src/assets/gifs/crying-dudu3.gif",
+  "src/assets/gifs/crying-dudu4.gif",
+];
+
+export const Sorries = [
+  "Naahi😒😠",
+  "Arrey 🥺",
+  "fir se soch lo 🥺",
+  "yorr, please??",
+  "arrey kardo na😣😖",
+  "Sahi ?",
+  "seriouslyyy ?",
+  "kutti kahi ki🥺",
+  "pleaseezz🤕",
+  "Tum hi galat sochti ho😑, aur serious ho jati ho",
+];
